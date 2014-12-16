25afd7b11c8df34bd1066300e0b8ac3b573ede6b
bandarenka <bond-94@ya.ru>
2014-12-16 22:24:44 +0300

    Little code separating

--- BankSystem/Main.cpp
+++ BankSystem/Main.cpp
@@ -1,5 +1,6 @@
 #define EXPORT_TEST_FUNCTIONS
 #include "Header.h"
+#include "Constants.h"
 #include "sqlite3.h"
 #include <iostream>
 #include <stdio.h>
@@ -16,11 +17,7 @@ void searchClient();
 void setClientActive();
 void setClientInactive();
 
-#define add_money "add money"
-#define withdraw_money "withdraw money"
-#define view_accounts "view accounts"
-#define login_string "Log In"
-#define exit "Exit "
+
 
 int admin_actions_count = 8;
 void(*admin_actions[])() =
@@ -52,29 +49,11 @@ char operator_actions_names[][128] =
 	"withdraw money"
 };
 
-#define operator_add_money 1
-#define operator_withdraw_money 2
-#define operator_exit 3
-#define login_action 1
-#define exit_action 2
+
 sqlite3 *conn;
 sqlite3 *conn_del;
 sqlite3_stmt * stmt;
-char* insert = "INSERT INTO client (CLIENT_ID,FIRST_NAME,LAST_NAME) VALUES (?, ?, ?);";
-char* insertAccount = "INSERT INTO ACCOUNT (CLIENT_ID) VALUES (?);";
-char* selectAccountById = "SELECT * FROM account WHERE account_id=?;";
-char* deleteAccountById = "DELETE FROM account WHERE account_id=?;";
-char* selectClientById = "SELECT * FROM client WHERE client_id=?;";
-char* selectAllClient = "SELECT * FROM client;";
-char* deleteClientById = "DELETE FROM client WHERE client_id=?;";
-char* deleteAccountByClientId = "DELETE FROM account WHERE client_id=?;";
-char* insertDeletedUser = "INSERT INTO deleted_clients (FIRST_NAME, LAST_NAME) VALUES (?,?);";
-char* selectAllDeleted = "SELECT * FROM deleted_clients;";
-char* selectAccountInfById = "SELECT client_id, balance, current_transactions FROM account WHERE account_id = ?;";
-char* insertDeletedAccount = "INSERT INTO deleted_account (ACCOUNT_ID, CLIENT_ID, BALANCE, TOTAL_TRANSACTIONS) VALUES (?,?,?,?);";
 
-char* setActive = "UPDATE client SET is_active=1 WHERE client_id=?;";
-char* setInactive = "UPDATE client SET is_active=0 WHERE client_id=?;";
                
 enum roleInSystem{UNKNOWN,ADMIN,OPERATOR} roleIdentified;
 int currentBalance;
